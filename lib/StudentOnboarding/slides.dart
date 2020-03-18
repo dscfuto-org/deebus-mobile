9ad727a62546a8de8173df9b91ecaa7ce817260e
Brendan Ejike <ejike.br@gmail.com>
2020-03-18 21:52:49 +0100

    Optimize & Reformat slides.dart

--- lib/StudentOnboarding/slides.dart
+++ lib/StudentOnboarding/slides.dart
@@ -3,67 +3,71 @@ import 'package:deebus/Constants/AssetsStrings.dart';
 import 'package:deebus/StudentOnboarding/UserAuthentication/signUp.dart';
 import 'package:deebus/Utils/Navigators.dart';
 import 'package:deebus/Utils/Styles.dart';
+import 'package:deebus/models/intro-screeen-model.dart';
 import 'package:flutter/material.dart';
-import 'package:intro_slider/intro_slider.dart';
-import 'package:intro_slider/slide_object.dart';
+import 'package:introduction_screen/introduction_screen.dart';
 
 class IntroScreen extends StatefulWidget {
-//  @override
   @override
   _IntroScreenState createState() => _IntroScreenState();
 }
 
 class _IntroScreenState extends State<IntroScreen> {
-  List<Slide> slides = new List();
+  // To hold the list of IntroScreen Widgets
+  List<PageViewModel> _pageViewModelList = List();
+
+  // To hold the list of IntroScreen models
+  List<WelcomeScreen> _screens = List();
+
+  PageViewModel _createIntroScreens(WelcomeScreen screen) {
+    return PageViewModel(
+      title: "", // Don not remove this, else Flutter will squawk!
+      bodyWidget: Column(
+        children: <Widget>[
+          Image.asset(
+            screen.image,
+            height: screen.imageHeight,
+            width: screen.imageWidth,
+          ),
+          Text(
+            screen.title,
+            style: textStyleBigBold,
+          ),
+          SizedBox(
+            height: 20.0,
+          ),
+          Text(
+            screen.description,
+            style: textStyleBigLight,
+            textAlign: TextAlign.center,
+          )
+        ],
+      ),
+    );
+  }
 
   @override
   void initState() {
     super.initState();
 
-    slides.add(
-      new Slide(
-        centerWidget: Column(
-          children: <Widget>[
-            Image.asset(firstVector, height: 300, width: 300,),
-            Text("Connect a Ride", style: textStyleBigBold,),
-            SizedBox(height: 20.0,),
-            Text(
-              "Check updates of rides around \n your standpoint to connect\n a ride",
-              style: textStyleBigLight, textAlign: TextAlign.center,)
-          ],
-        ),
-          backgroundColor: AppColors.color1
-      ),
-    );
-    slides.add(
-      new Slide(
-        centerWidget: Column(
-          children: <Widget>[
-            Image.asset(secondVector, height: 300, width: 300,),
-            Text("Complete Connect", style: textStyleBigBold,),
-            SizedBox(height: 20.0,),
-            Text(
-              "Input a complete details of your\njourney and get to join the ride",
-              style: textStyleBigLight, textAlign: TextAlign.center)
-          ],
-        ),
-          backgroundColor: AppColors.color1
-      ),
-    );
-    slides.add(
-      new Slide(
-        centerWidget: Column(
-          children: <Widget>[
-            Image.asset(thirdVector, height: 300, width: 300,),
-            Text("Get on Board", style: textStyleBigBold,),
-            SizedBox(height: 20.0,),
-            Text("Your ride meets you at the pick up\npoint of choice",
-              style: textStyleBigLight, textAlign: TextAlign.center)
-          ],
-        ),
-      backgroundColor: AppColors.color1
-      ),
-    );
+    _screens.add(WelcomeScreen(
+        image: firstVector,
+        title: "Connect a Ride",
+        description:
+            "Check updates of rides around \n your standpoint to connect\n a ride"));
+    _screens.add(WelcomeScreen(
+        image: secondVector,
+        title: "Complete Connect",
+        description:
+            "Input a complete details of your\njourney and get to join the ride"));
+    _screens.add(WelcomeScreen(
+        image: thirdVector,
+        title: "Get on Board",
+        description: "Your ride meets you at the pick up\npoint of choice"));
+
+    // Foreach of the above instances, create an IntroScreen & it to the PageViewModel List
+    _screens
+        .forEach((elem) => _pageViewModelList.add(_createIntroScreens(elem)));
   }
 
   void onDonePress() {
@@ -73,29 +77,31 @@ class _IntroScreenState extends State<IntroScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return new IntroSlider(
-      slides: this.slides,
-      colorActiveDot: AppColors.color4,
-      onDonePress: this.onDonePress,
-      backgroundColorAllSlides: AppColors.color1,
-      sizeDot: 10.0,
-      nameDoneBtn: "Got it",
-      styleNameDoneBtn: TextStyle(
-        color: AppColors.color4,
-        fontSize: 15.0,
-          fontWeight: FontWeight.bold
+    return IntroductionScreen(
+      pages: _pageViewModelList,
+      initialPage: 0,
+      showSkipButton: true,
+      skip: Text("Skip", style: introBtnsTextStyle),
+      showNextButton: true,
+      next: Text(
+        "Next",
+        style: introBtnsTextStyle,
       ),
-      nameNextBtn: "Next",
-      nameSkipBtn: "Skip",
-      onSkipPress: this.onDonePress,
-      styleNameSkipBtn: TextStyle(
-        color: AppColors.color4,
-        fontSize: 15.0,
-        fontWeight: FontWeight.bold
+      done: Text(
+        "Done",
+        style: introBtnsTextStyle,
       ),
-      isShowSkipBtn: true,
-
-
+      dotsFlex: 2,
+      globalBackgroundColor: AppColors.color1,
+      onDone: () => this.onDonePress(),
+      dotsDecorator: DotsDecorator(
+          size: const Size.square(10.0),
+          activeSize: const Size(20.0, 10.0),
+          activeColor: Theme.of(context).accentColor,
+          color: Colors.black26,
+          spacing: const EdgeInsets.symmetric(horizontal: 3.0),
+          activeShape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(25.0))),
     );
   }
 }
--- lib/Utils/Styles.dart
+++ lib/Utils/Styles.dart
@@ -1,18 +1,25 @@
-
 import 'package:deebus/Constants/AppColors.dart' as prefix0;
+import 'package:deebus/Constants/AppColors.dart';
 import 'package:flutter/material.dart';
 
-
-TextStyle textStyleBigLight = new TextStyle(fontSize: 16.0, fontWeight: FontWeight.w100, fontFamily: 'Ubuntu');
+TextStyle textStyleBigLight = new TextStyle(
+    fontSize: 16.0, fontWeight: FontWeight.w300, fontFamily: 'Ubuntu');
 
 //TextStyle textStyleSmallLight = new TextStyle(fontSize: 14.0, fontFamily: 'Ubuntu');
 
-TextStyle textStyleBigBold = new TextStyle(fontSize: 30.0, fontWeight: FontWeight.w700, fontFamily: 'Ubuntu');
+TextStyle textStyleBigBold = new TextStyle(
+    fontSize: 30.0, fontWeight: FontWeight.w700, fontFamily: 'Ubuntu');
 
-
-TextStyle textStyleSmallRegular = new TextStyle(fontSize: 14.0, fontFamily: 'Ubuntu');
+TextStyle textStyleSmallRegular =
+    new TextStyle(fontSize: 14.0, fontFamily: 'Ubuntu');
 
 TextStyle textStyleBigRegular = new TextStyle(fontSize: 16.0);
 
-TextStyle textStyleBigLightB = new TextStyle(fontSize: 16.0, fontWeight: FontWeight.w100, fontFamily: 'Ubuntu', color: prefix0.AppColors.color4);
+TextStyle textStyleBigLightB = new TextStyle(
+    fontSize: 16.0,
+    fontWeight: FontWeight.w100,
+    fontFamily: 'Ubuntu',
+    color: prefix0.AppColors.color4);
 
+TextStyle introBtnsTextStyle = TextStyle(
+    color: AppColors.color4, fontSize: 15.0, fontWeight: FontWeight.bold);
