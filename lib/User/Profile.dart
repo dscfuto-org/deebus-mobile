170c65179bd4601c1fea8f9baf172c772361556b
f0rx <ejike.br@gmail.com>
2020-03-19 18:14:38 +0100

    Add Profile Screen

---
+++ lib/User/Profile.dart
@@ -0,0 +1,52 @@
+import 'package:deebus/Constants/AppColors.dart';
+import 'package:deebus/Utils/Navigators.dart';
+import 'package:flutter/material.dart';
+
+class ProfileScreen extends StatefulWidget {
+  @override
+  _ProfileScreenState createState() => _ProfileScreenState();
+}
+
+class _ProfileScreenState extends State<ProfileScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Odii Marshall"),
+        backgroundColor: Colors.white10,
+        elevation: 0,
+        leading: IconButton(
+          icon: Icon(
+            Icons.arrow_back,
+            color: AppColors.color4,
+            size: 35.0,
+          ),
+          onPressed: () => navigateBack(context),
+        ),
+        actions: <Widget>[
+          FlatButton(
+            onPressed: () {},
+            color: Colors.white10,
+            child: Text(
+              "LOG OUT",
+              style: TextStyle(fontWeight: FontWeight.w300, fontSize: 15.0),
+            ),
+          )
+        ],
+      ),
+      body: SingleChildScrollView(
+        scrollDirection: Axis.vertical,
+        physics: ClampingScrollPhysics(),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            RaisedButton(
+              child: Text("CONNECT TO FACEBOOK"),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
